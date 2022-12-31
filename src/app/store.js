70b4b93f4d43d38fa4400f68833f57603a2a48eb
Abhinandan <abhinandang.cs.20@nitj.ac.in>
2022-12-31 22:14:30 +0530

    Checkout Page done

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import basketReducer from "../slices/basketSlice";
 
+// THE GLOBAL STORE SETUP REDUX
 export const store = configureStore({
   reducer: {
     basket: basketReducer,
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,23 @@
 import Image from "next/image"
 import { MagnifyingGlassIcon, ShoppingCartIcon, Bars3Icon } from "@heroicons/react/24/outline"
 import { signIn, signOut, useSession } from "next-auth/react"
+import { useRouter } from "next/router";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
 
 function Header() {
 
     const {data:session} = useSession();
+    const router = useRouter();
+    const items = useSelector(selectItems);
+
   return (
     <header>
         {/* top navbar container */}
         <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
             {/* Amazon Logo */}
             <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
-                <Image src="https://links.papareact.com/f90" width={150} height={40} objectFit="contain" className="cursor-pointer" />
+                <Image onClick={() => router.push('/')} src="https://links.papareact.com/f90" width={150} height={40} objectFit="contain" className="cursor-pointer" />
             </div>
 
             {/* Search Bar */}
@@ -37,8 +43,8 @@ function Header() {
                 </div>
 
                 {/* basket */}
-                <div className="relative cursor-pointer hover:underline flex items-center">
-                    <span className="absolute top-0 right-0 md:right-10 bg-yellow-400 h-4 w-4 text-center rounded-full text-black font-bold">0</span>
+                <div onClick={() => router.push('/checkout')} className="relative cursor-pointer hover:underline flex items-center">
+                    <span className="absolute top-0 right-0 md:right-10 bg-yellow-400 h-4 w-4 text-center rounded-full text-black font-bold">{items.length}</span>
                     <ShoppingCartIcon className="h-10"/>
                     <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
                 </div>
